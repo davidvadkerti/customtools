2d35072df109730db00ca2355c3c4ea1d1a737a4
davidvadkerti <davidvadkerti@gmail.com>
2021-11-26 15:23:06 +0100

    fix of the error when IFC file is linked or reloaded

--- hooks/doc-opened.py
+++ hooks/doc-opened.py
@@ -6,82 +6,83 @@ from pyrevit.userconfig import user_config
 from customOutput import def_openingLogPath
 
 doc = __eventargs__.Document
-filePath = doc.PathName
+# linking IFC models leads to an error because of no document
+if doc:
+    filePath = doc.PathName
+    # getting central file name for log name
+    central_path = revit.query.get_central_path(doc)
+    fileExtension = filePath[-3:]
 
-# getting central file name for log name
-central_path = revit.query.get_central_path(doc)
-fileExtension = filePath[-3:]
-
-if fileExtension == "rvt":
-    # GETTING FILE NAME
-    # getting local file name for tmp file name
-    try:
-        lastBackslash_L = filePath.rindex("\\")
-    except:
-        # for opened as dettached file
-        lastBackslash_L = filePath.rindex("/")
-
-    # just the file name without the extension
-    local_file_name = filePath[lastBackslash_L:][:-4]
-
-    # skipped if file is not workshared (when slash is not in filepath)
-    # if central_path != None or central_path !="":
-    try:
+    if fileExtension == "rvt":
+        # GETTING FILE NAME
+        # getting local file name for tmp file name
         try:
-            # for rvt server
-            lastBackslash_C = central_path.rindex("/")
+            lastBackslash_L = filePath.rindex("\\")
         except:
-            # for other locations
-            lastBackslash_C = central_path.rindex("\\")
+            # for opened as dettached file
+            lastBackslash_L = filePath.rindex("/")
+
         # just the file name without the extension
-        central_file_name = central_path[lastBackslash_C:][:-4]
-    except:
-        central_file_name = local_file_name
+        local_file_name = filePath[lastBackslash_L:][:-4]
 
-    # LOGGING
-    # tabulator between data to separte columns of the schedule
-    separator = "\t" 
-    try:
-        # reading timestamp from tmp file
+        # skipped if file is not workshared (when slash is not in filepath)
+        # if central_path != None or central_path !="":
         try:
-            # if parameter exists in config file
             try:
-                openingLogPath = user_config.CustomToolsSettings.openingLogPath
-            # if parameter doesnt exist in config file 
+                # for rvt server
+                lastBackslash_C = central_path.rindex("/")
             except:
-                openingLogPath = def_openingLogPath
-            tmp_file_path = openingLogPath + "\\"+ local_file_name + "_Open.tmp"
-            # tmp_file_path = "L:\\customToolslogs\\openingTimeLogs\\"+ local_file_name + "_Open.tmp"
+                # for other locations
+                lastBackslash_C = central_path.rindex("\\")
+            # just the file name without the extension
+            central_file_name = central_path[lastBackslash_C:][:-4]
         except:
-            tmp_file_path = " \\\\Srv2\\Z\\customToolslogs\\openingTimeLogs\\"+ local_file_name + "_Open.tmp"
-        tmp_file = open(tmp_file_path, "r")
-        start_time_string = tmp_file.read()
-        # converting string to datetime
-        start_time = datetime.strptime(start_time_string,"%Y-%m-%d %H:%M:%S")
-        tmp_file.close()
+            central_file_name = local_file_name
 
-        if path.exists(tmp_file_path):
-            remove(tmp_file_path)
-
-        # end time in seconds
-        # round datetime to seconds (converting to string and then back to datetime object with correct)
-        end_time_string_seconds = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        end_time = datetime.strptime(end_time_string_seconds,"%Y-%m-%d %H:%M:%S")
-
-
-        timeDelta = end_time - start_time
-        # print timeDelta
-
-        user_name = doc.Application.Username
-
-        # writing time to log file
-        # if openingLogPath exist
+        # LOGGING
+        # tabulator between data to separte columns of the schedule
+        separator = "\t" 
         try:
-            log_file = open(openingLogPath + "\\"+ central_file_name + "_Open.log", "a")
-            # log_file = open("L:\\customToolslogs\\openingTimeLogs\\"+ central_file_name + "_Open.log", "a")
+            # reading timestamp from tmp file
+            try:
+                # if parameter exists in config file
+                try:
+                    openingLogPath = user_config.CustomToolsSettings.openingLogPath
+                # if parameter doesnt exist in config file 
+                except:
+                    openingLogPath = def_openingLogPath
+                tmp_file_path = openingLogPath + "\\"+ local_file_name + "_Open.tmp"
+                # tmp_file_path = "L:\\customToolslogs\\openingTimeLogs\\"+ local_file_name + "_Open.tmp"
+            except:
+                tmp_file_path = " \\\\Srv2\\Z\\customToolslogs\\openingTimeLogs\\"+ local_file_name + "_Open.tmp"
+            tmp_file = open(tmp_file_path, "r")
+            start_time_string = tmp_file.read()
+            # converting string to datetime
+            start_time = datetime.strptime(start_time_string,"%Y-%m-%d %H:%M:%S")
+            tmp_file.close()
+
+            if path.exists(tmp_file_path):
+                remove(tmp_file_path)
+
+            # end time in seconds
+            # round datetime to seconds (converting to string and then back to datetime object with correct)
+            end_time_string_seconds = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            end_time = datetime.strptime(end_time_string_seconds,"%Y-%m-%d %H:%M:%S")
+
+
+            timeDelta = end_time - start_time
+            # print timeDelta
+
+            user_name = doc.Application.Username
+
+            # writing time to log file
+            # if openingLogPath exist
+            try:
+                log_file = open(openingLogPath + "\\"+ central_file_name + "_Open.log", "a")
+                # log_file = open("L:\\customToolslogs\\openingTimeLogs\\"+ central_file_name + "_Open.log", "a")
+            except:
+                log_file = open("\\\\Srv2\\Z\\customToolslogs\\openingTimeLogs\\"+ central_file_name + "_Open.log", "a")
+            log_file.write(end_time_string_seconds + separator + str(timeDelta)+ separator + user_name + "\n")
+            log_file.close()
         except:
-            log_file = open("\\\\Srv2\\Z\\customToolslogs\\openingTimeLogs\\"+ central_file_name + "_Open.log", "a")
-        log_file.write(end_time_string_seconds + separator + str(timeDelta)+ separator + user_name + "\n")
-        log_file.close()
-    except:
-         pass
+             pass
