066fa1f7063271035f0a68c614a3dec48ffb4a29
davidvadkerti <davidvadkerti@gmail.com>
2021-12-31 13:06:10 +0100

    New feature - Window & Door Swing Setter -  automatic setting detection of Door and Window FacingFlipped and HandFlipped values

---
+++ hooks/doc-updater.py
@@ -0,0 +1,58 @@
+# -*- coding: UTF-8 -*-
+from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory, \
+        Transaction, Document, BuiltInParameter
+from pyrevit import revit, EXEC_PARAMS
+
+doc = EXEC_PARAMS.event_doc
+
+left = "L"
+right = "R"
+
+# Auto Window flip setter
+windows = FilteredElementCollector(doc) \
+        .OfCategory(BuiltInCategory.OST_Windows) \
+        .WhereElementIsNotElementType()
+param_name = 'Window Swing'
+
+
+with revit.Transaction('Auto Window Flip Setter'):
+    for w in windows:
+        w_type_Id = w.GetTypeId()
+        w_type = Document.GetElement(doc,w_type_Id)
+        # skipping the wall penetration modelled in the window category
+        filterp = w_type.get_Parameter(BuiltInParameter.ALL_MODEL_TYPE_COMMENTS)
+        if filterp and filterp.AsString() != "stavebne upravy":
+            param = w.LookupParameter(param_name)
+            if param:
+                # default window is Right
+                if w.HandFlipped:
+                    if w.FacingFlipped:
+                        param.Set(right)
+                    else:
+                        param.Set(left)
+                else:
+                    if w.FacingFlipped:
+                        param.Set(left)
+                    else:
+                        param.Set(right)
+
+# Auto Door flip setter
+doors = FilteredElementCollector(doc) \
+        .OfCategory(BuiltInCategory.OST_Doors) \
+        .WhereElementIsNotElementType()
+param_name = 'Door Swing'
+with revit.Transaction('Auto Door Flip Setter'):
+    for d in doors:
+        param = d.LookupParameter(param_name)
+        if param:
+            # default door is Left
+            if d.HandFlipped:
+                if d.FacingFlipped:
+                    param.Set(left)
+                else:
+                    param.Set(right)
+            else:
+                if d.FacingFlipped:
+                    param.Set(right)
+                else:
+                    param.Set(left)
