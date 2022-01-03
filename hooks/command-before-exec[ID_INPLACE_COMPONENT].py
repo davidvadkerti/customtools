19c81011e53f115c3344c58e8ef904ac5348b261
davidvadkerti <davidvadkerti@gmail.com>
2022-01-03 17:32:00 +0100

    Hook Translation - added file with texts in various languages (just SK and EN in this version)

--- hooks/command-before-exec[ID_INPLACE_COMPONENT].py
+++ hooks/command-before-exec[ID_INPLACE_COMPONENT].py
@@ -2,30 +2,33 @@
 from pyrevit import EXEC_PARAMS
 from pyrevit import forms, script
 
+from hook_translate import hook_texts
+
 doc = __revit__.ActiveUIDocument.Document
 
-res = forms.alert("POZOR!\n\n"
-                  "In Place Families by mali byť použité len vo výnimočných prípadoch, "
-                  "keďže majú oproti Loadable Families veľa nevýhod: \n"
-                  " - problematické vykazovanie\n"
-                  " - nemajú identifikáciu o levele, na ktorom sú umiestnené\n"
-                  " - pri skopírovaní prvku vzniká nový nezávislý originál\n"
-                  " - nemožnosť modelovať parametricky\n\n"
-                  "Chceš naozaj vytvoriť In Place Family?",
-                  options=["Vytvoriť",
-                           "Zrušiť",
-                           "Viac info o In Place Families"],
-                  title="In Place Family",
-                  footer="CustomTools Hooks")
-if res  == "Vytvoriť":
+title = "In Place Family"
+# the language value will be read from the pyRevit config file - I will add this later
+lang = "EN"
+
+# WARNING WINDOW
+res = forms.alert(hook_texts[lang][title]["text"],
+                  options = hook_texts[lang][title]["buttons"],
+                  title = title,
+                  footer = "CustomTools Hooks")
+
+# BUTTONS
+# Create
+if res  == hook_texts[lang][title]["buttons"][0]:
    EXEC_PARAMS.event_args.Cancel = False
    # logging to server
    from hooksScripts import hooksLogger
    hooksLogger("Inplace Component", doc)
 
-elif res  == "Zrušiť":
+# Cancel
+elif res  == hook_texts[lang][title]["buttons"][1]:
    EXEC_PARAMS.event_args.Cancel = True
-elif res  == "Viac info o In Place Families":
+# More info
+elif res  == hook_texts[lang][title]["buttons"][2]:
    EXEC_PARAMS.event_args.Cancel = True
    url = 'https://gfi.miraheze.org/wiki/In-place_Families'
    script.open_url(url)
---
+++ lib/hook_translate.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+hook_texts = {
+
+  # SLOVAK
+  "SK":{
+    # IN PLACE FAMILIES
+    "In Place Family":{
+"text":
+"POZOR!\n\n\
+In Place Families by mali byť použité len vo výnimočných prípadoch,\n\
+keďže majú oproti Loadable Families veľa nevýhod: \n\
+- problematické vykazovanie\n\
+- nemajú identifikáciu o levele, na ktorom sú umiestnené\n\
+- pri skopírovaní prvku vzniká nový nezávislý originál\n\
+- nemožnosť modelovať parametricky\n\n\
+Chceš naozaj vytvoriť In Place Family?",
+        "buttons":
+        ["Vytvoriť",
+        "Zrušiť",
+        "Viac info o In Place Families"],
+    },
+    # ROOF EXTRUSION
+    "Roof Extrusion":{
+        "text":"this is roof text",
+        "buttons":"these are roof extrusion buttons",
+    },
+  },
+
+  # ENGLISH
+  "EN":{
+    # IN PLACE FAMILIES
+    "In Place Family":{
+        "text":
+"WARNING!\n\n\
+In Place Families should be used only in exceptional cases,\n\
+hence they have lot of disadvantages:\n\
+- have problematic scheduling options\n\
+- have no Level asigned\n\
+- each copy is independent family instance\n\
+- have limited support for parametric modeling\n\n\
+Do you really want create In Place Family?",
+        "buttons":
+        ["Create",
+        "Cancel",
+        "More info about In Place Families"],
+    },
+    # ROOF EXTRUSION
+    "Roof Extrusion":{
+        "text":"this is roof text",
+        "buttons":"these are roof extrusion buttons",
+    },
+  },
+}
