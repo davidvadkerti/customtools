a66cecca548c837d9cf398baf6af617fa30289f9
davidvadkerti <davidvadkerti@gmail.com>
2022-01-04 02:00:06 +0100

    command-before-exec[ID_FILE_IMPORT].py - hook translation implemented

--- hooks/command-before-exec[ID_FILE_IMPORT].py
+++ hooks/command-before-exec[ID_FILE_IMPORT].py
@@ -9,27 +9,30 @@ docName = doc.PathName
 fileExtension = docName[-3:]
 
 def dialogBox():
+   from hook_translate import hook_texts, lang
+   title = "Import CAD file"
+   # the language value is read from pyrevit config file
+   lang = lang()
+
    # for not showing the hook in families - not saved families have no extension
    if fileExtension == "rvt" or fileExtension == "rte":
-      res = forms.alert("POZOR!\n\n"
-                       "Importovať CAD súbory by si mal len výnimočne.\n"
-                        "Nikdy neimportuj CAD priamo do modelu, ale do čistého RVT súboru.\n\n"
-                        "Si si istý, že vieš, čo robíš?",
-                        title="Import CAD file",
-                        footer="CustomTools Hooks",
-                        options=["Importovať",
-                                 "Zrušiť",
-                                 "Viac info o Importovaní CAD súborov"])
+      res = forms.alert(hook_texts[lang][title]["text"],
+                  options = hook_texts[lang][title]["buttons"],
+                  title = title,
+                  footer = "CustomTools Hooks")
 
-      if res  == "Importovať":
+      # Import
+      if res  == hook_texts[lang][title]["buttons"][0]:
          EXEC_PARAMS.event_args.Cancel = False
          # logging to server
          from hooksScripts import hooksLogger
          hooksLogger("CAD file import", doc)
 
-      elif res  == "Zrušiť":
+      # Cancel
+      elif res  == hook_texts[lang][title]["buttons"][1]:
          EXEC_PARAMS.event_args.Cancel = True
-      elif res  == "Viac info o Importovaní CAD súborov":
+      # More info
+      elif res  == hook_texts[lang][title]["buttons"][2]:
          EXEC_PARAMS.event_args.Cancel = True
          url = 'https://gfi.miraheze.org/wiki/Postupy,_ktor%C3%BDm_je_potrebn%C3%A9_sa_vyhn%C3%BA%C5%A5_-_Revit#Importovanie_DWG'
          script.open_url(url)
