d475b2211ca10a48c9889b7877a87a8fde318b8f
davidvadkerti <davidvadkerti@gmail.com>
2022-01-10 10:34:43 +0100

    Help - link changed to notion if landuage is not Slovak

--- CustomTools.tab/Help.panel/Help links.stack/Help.pushbutton/script.py
+++ CustomTools.tab/Help.panel/Help links.stack/Help.pushbutton/script.py
@@ -1,6 +1,11 @@
 from pyrevit import script
+from hook_translate import lang
 
 __context__ = 'zero-doc'
+lang = lang()
 
-url = 'https://gfi.miraheze.org/wiki/CustomTools_(Extension_pre_pyRevit)'
+if lang == "SK":
+	url = 'https://gfi.miraheze.org/wiki/CustomTools_(Extension_pre_pyRevit)'
+else:
+	url = 'https://customtools.notion.site/customtools/CustomTools-wiki-76d8472edc6444e5bb3ce90f7998f1ef'
 script.open_url(url)
