59faf14061af64e26fb1949f5ecbc13e8e792f13
davidvadkerti <davidvadkerti@gmail.com>
2022-01-18 12:54:54 +0100

    Door & Window Swing - option to turn the swing setter off separately for windows and doors when Y/N parameter is added and checked

--- hooks/doc-updater.py
+++ hooks/doc-updater.py
@@ -8,7 +8,23 @@ doc = EXEC_PARAMS.event_doc
 left = "L"
 right = "P"
 
-if not doc.IsFamilyDocument:
+def door_swing_setter(doc):
+    # Auto Door flip setter
+    doors = FilteredElementCollector(doc) \
+            .OfCategory(BuiltInCategory.OST_Doors) \
+            .WhereElementIsNotElementType()
+    param_name = 'Door Swing'
+    with revit.Transaction('Auto Door Flip Setter'):
+        for d in doors:
+            param = d.LookupParameter(param_name)
+            if param:
+                # # default door is Left
+                if d.Mirrored:
+                    param.Set(right)
+                else:
+                    param.Set(left)
+
+def win_swing_setter(doc):
     # Auto Window flip setter
     windows = FilteredElementCollector(doc) \
             .OfCategory(BuiltInCategory.OST_Windows) \
@@ -29,17 +45,30 @@ if not doc.IsFamilyDocument:
                     else:
                         param.Set(right)
 
-    # Auto Door flip setter
-    doors = FilteredElementCollector(doc) \
-            .OfCategory(BuiltInCategory.OST_Doors) \
-            .WhereElementIsNotElementType()
-    param_name = 'Door Swing'
-    with revit.Transaction('Auto Door Flip Setter'):
-        for d in doors:
-            param = d.LookupParameter(param_name)
-            if param:
-                # # default door is Left
-                    if d.Mirrored:
-                        param.Set(right)
-                    else:
-                        param.Set(left)
+
+if not doc.IsFamilyDocument:
+    # WINDOWS
+    # getting project info
+    # if there is shared parameter NOT checked, script is not triggered
+    win_settings = doc.ProjectInformation.LookupParameter("ctWindowSwingSetterOFF")
+    try:
+        # if parameter is turned on
+        if not win_settings or win_settings.AsInteger() == 0:
+            win_swing_setter(doc)
+    except AttributeError:
+        # if parameter doesn't exist
+        if not win_settings:
+            win_swing_setter(doc)
+
+    # DOORS
+    # getting project info
+    # if there is shared parameter NOT checked, script is not triggered
+    door_settings = doc.ProjectInformation.LookupParameter("ctDoorSwingSetterOFF")
+    try:
+        # if parameter is nurned on
+        if not door_settings or door_settings.AsInteger() == 0:
+            door_swing_setter(doc)
+    except AttributeError:
+        # if parameter doesn't exist
+        if not door_settings:
+            door_swing_setter(doc)
