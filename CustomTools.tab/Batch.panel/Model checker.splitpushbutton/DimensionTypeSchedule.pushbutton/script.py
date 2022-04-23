b777cc2166ab275117c1edfd0bda1d1f507b292e
davidvadkerti <davidvadkerti@gmail.com>
2022-04-23 12:11:55 +0200

    Dimension Type Schedule - adding sorting options and renaming variables

--- CustomTools.tab/Batch.panel/Model checker.splitpushbutton/DimensionTypeSchedule.pushbutton/script.py
+++ CustomTools.tab/Batch.panel/Model checker.splitpushbutton/DimensionTypeSchedule.pushbutton/script.py
@@ -5,7 +5,7 @@ from pyrevit import forms
 from pyrevit import output
 
 from Autodesk.Revit.DB import FilteredElementCollector
-from Autodesk.Revit.DB import RevisionCloud, Revision, TextNoteType, TextNote, Dimension, DimensionType
+from Autodesk.Revit.DB import RevisionCloud, Revision, Dimension, DimensionType
 
 from pyrevit.coreutils import Timer
 from customOutput import hmsTimer, ct_icon, file_name_getter
@@ -17,68 +17,91 @@ output.set_width(1300)
 # changing icon
 ct_icon(output)
 
-timer = Timer()
-# heading
-output.print_md("# DIMENSION TYPE SCHEDULE")
 
-textNoteType_collector = FilteredElementCollector(doc).OfClass(DimensionType).ToElements()
-textNote_collector = FilteredElementCollector(doc).OfClass(Dimension).WhereElementIsNotElementType().ToElements()
+def showDimensionSchedule(sortByParam):
+    timer = Timer()
+    # heading
+    output.print_md("# DIMENSION TYPE SCHEDULE")
 
-# count of instances per type
-text_note_types = {}
-for tn in textNote_collector:
-    tn_type_id = tn.GetTypeId().ToString()
-    try:
-        text_note_types[tn_type_id] += 1
-    except:
-        text_note_types[tn_type_id] = 1
+    dimensionType_collector = FilteredElementCollector(doc).OfClass(DimensionType).ToElements()
+    dimension_collector = FilteredElementCollector(doc).OfClass(Dimension).WhereElementIsNotElementType().ToElements()
 
-# text notes type parameters
-scheduleData = []
-for text_note in textNoteType_collector:
-    text_note_name = text_note.get_Parameter(DB.BuiltInParameter.ALL_MODEL_TYPE_NAME).AsString()
-    font = text_note.get_Parameter(DB.BuiltInParameter.TEXT_FONT).AsString()
-    size = text_note.get_Parameter(DB.BuiltInParameter.TEXT_SIZE).AsDouble() * 304.8
-    bold = text_note.get_Parameter(DB.BuiltInParameter.TEXT_STYLE_BOLD).AsInteger()
-    background = text_note.get_Parameter(DB.BuiltInParameter.DIM_TEXT_BACKGROUND).AsInteger()
-    show_opening_ht = text_note.get_Parameter(DB.BuiltInParameter.DIM_STYLE_SHOW_OPENING_HT).AsInteger()
-    width_factor = text_note.get_Parameter(DB.BuiltInParameter.TEXT_WIDTH_SCALE).AsDouble()
-    style_type = text_note.StyleType
-    wtns_ln_ctrl = text_note.get_Parameter(DB.BuiltInParameter.DIM_WITNS_LINE_CNTRL).AsInteger()
-    if wtns_ln_ctrl == 0:
-        wtns_ln_ctrl_v = "Gap to Element"
-    if wtns_ln_ctrl == 1:
-        wtns_ln_ctrl_v = "Fixed"
+    # count of instances per type
+    dimension_types = {}
+    for tn in dimension_collector:
+        tn_type_id = tn.GetTypeId().ToString()
+        try:
+            dimension_types[tn_type_id] += 1
+        except:
+            dimension_types[tn_type_id] = 1
 
-    witns_ln_gap = text_note.get_Parameter(DB.BuiltInParameter.WITNS_LINE_GAP_TO_ELT).AsDouble() * 304.8
-    witns_ln_ext = text_note.get_Parameter(DB.BuiltInParameter.WITNS_LINE_EXTENSION).AsDouble() * 304.8
+    # text notes type parameters
+    scheduleData = []
+    for dimension in dimensionType_collector:
+        dimension_name = dimension.get_Parameter(DB.BuiltInParameter.ALL_MODEL_TYPE_NAME).AsString()
+        font = dimension.get_Parameter(DB.BuiltInParameter.TEXT_FONT).AsString()
+        size = dimension.get_Parameter(DB.BuiltInParameter.TEXT_SIZE).AsDouble() * 304.8
+        bold = dimension.get_Parameter(DB.BuiltInParameter.TEXT_STYLE_BOLD).AsInteger()
+        background = dimension.get_Parameter(DB.BuiltInParameter.DIM_TEXT_BACKGROUND).AsInteger()
+        show_opening_ht = dimension.get_Parameter(DB.BuiltInParameter.DIM_STYLE_SHOW_OPENING_HT).AsInteger()
+        width_factor = dimension.get_Parameter(DB.BuiltInParameter.TEXT_WIDTH_SCALE).AsDouble()
+        style_type = dimension.StyleType
+        wtns_ln_ctrl = dimension.get_Parameter(DB.BuiltInParameter.DIM_WITNS_LINE_CNTRL).AsInteger()
+        if wtns_ln_ctrl == 0:
+            wtns_ln_ctrl_v = "Gap to Element"
+        if wtns_ln_ctrl == 1:
+            wtns_ln_ctrl_v = "Fixed"
+
+        witns_ln_gap = dimension.get_Parameter(DB.BuiltInParameter.WITNS_LINE_GAP_TO_ELT).AsDouble() * 304.8
+        witns_ln_ext = dimension.get_Parameter(DB.BuiltInParameter.WITNS_LINE_EXTENSION).AsDouble() * 304.8
 
 
-    text_note_id = text_note.Id
-    text_note_type_creator = DB.WorksharingUtils.GetWorksharingTooltipInfo(revit.doc, text_note_id).Creator 
-    try:
-        count = text_note_types[text_note_id.ToString()]
-    except:
-        count = 0
+        dimension_id = dimension.Id
+        dimension_type_creator = DB.WorksharingUtils.GetWorksharingTooltipInfo(revit.doc, dimension_id).Creator 
+        try:
+            count = dimension_types[dimension_id.ToString()]
+        except:
+            count = 0
 
-    # list only items with names not settings items
-    if text_note_name:
-        paramList = [text_note_name, font, size, bold, background, show_opening_ht, width_factor, wtns_ln_ctrl_v, witns_ln_gap, witns_ln_ext, style_type, output.linkify(text_note_id), count, text_note_type_creator]
+        # list only items with names not settings items
+        if dimension_name:
+            paramList = [dimension_name, font, size, bold, background, show_opening_ht, width_factor, wtns_ln_ctrl_v, witns_ln_gap, witns_ln_ext, style_type, output.linkify(dimension_id), count, dimension_type_creator]
 
-        scheduleData.append(paramList)
+            scheduleData.append(paramList)
 
-# sort by parameters
-sortedScheduleData = sorted(scheduleData, key=lambda x: int(x[12]))
+    # sort by parameters
+    if sortByParam == "By Style Type":
+        sortedScheduleData = sorted(scheduleData, key=lambda x: int(x[10]))
+    elif sortByParam == "By Creator":
+        sortedScheduleData = sorted(scheduleData, key=lambda x: x[13].lower())
+    elif sortByParam == "By Dimension Type Name":
+        sortedScheduleData = sorted(scheduleData, key=lambda x: x[0].lower())
+    else:
+        sortedScheduleData = sorted(scheduleData, key=lambda x: int(x[12]))
 
-# printing the schedule if there are data
-if sortedScheduleData:
-    output.print_table(table_data=sortedScheduleData,
-                       title = file_name_getter(doc),
-                       columns=["Text Note Type", "Font", "Size", "Bold", "Background", "Text Box Visibility" ,"Width Factor" ,"Witness line extension", "Witness Line Gap", "Witness line extension", "Style Type","Text Note Type ID", "Count", "Creator"],
-                       formats=['', '', '', '', '', '', '', '', '', '', '', '', ''])
-# if there are no data print status claim
-else:
-    print("There are no Dimension Types in the Project")
-  # for timing------
-endtime = timer.get_time()
-print(hmsTimer(endtime))
+    # printing the schedule if there are data
+    if sortedScheduleData:
+        output.print_table(table_data=sortedScheduleData,
+                           title = file_name_getter(doc),
+                           columns=["Dimension Type", "Font", "Size", "Bold", "Background", "Text Box Visibility" ,"Width Factor" ,"Witness line extension", "Witness Line Gap", "Witness line extension", "Style Type","Text Note Type ID", "Count", "Creator"],
+                           formats=['', '', '', '', '', '', '', '', '', '', '', '', ''])
+    # if there are no data print status claim
+    else:
+        print("There are no Dimension Types in the Project")
+      # for timing------
+    endtime = timer.get_time()
+    print(hmsTimer(endtime))
+
+my_config = script.get_config()
+
+selected_option = \
+    forms.CommandSwitchWindow.show(
+        ['By Style Type',
+         'By Dimension Type Name',
+         'By Count of Instances',
+         'By Creator'],
+        message='Sort by:'
+        )
+
+if selected_option:
+    showDimensionSchedule(selected_option)
